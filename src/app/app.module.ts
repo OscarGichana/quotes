ab3c5049193736babb3801a32d5c0c24f9fcf45d
Oscar Gichana <gichanaoscar3@gmail.com>
2021-01-31 18:15:34 +0300

    adding quotes component - using ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter } from'@angular/core';
+import { Quote } from '../shared/quotes';
+
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
