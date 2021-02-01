793e92df01ed458358d2126d475f8c1d1a49fe3c
Oscar Gichana <gichanaoscar3@gmail.com>
2021-02-01 20:52:10 +0300

    Auto-generated commit

--- src/app/shared/quotes.ts
+++
@@ -1,9 +0,0 @@
-export class Quote {
-  public upVotes:number
-  public downVotes:number
-  public showDescription: boolean;
-  constructor(public id: number, public quote: string, public name: string, public author: string, public completeDate: Date){
-    this.showDescription=false;
-
-  }
-}
